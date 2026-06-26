47ad0f03f878b125bfccf0d363495b4f5d474a16
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-06-26 12:20:38 -0400

    email function fixed

--- middlewares/sendEmail.js
+++ middlewares/sendEmail.js
@@ -1,18 +1,24 @@
 import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  await transporter.sendMail({
-    from: `"Stock Management Support" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    const info = await transporter.sendMail({
+      from: `"Stock Management Support" <${process.env.EMAIL_USER}>`,
+      to: ["hadyafardin@gmail.com", "tech.yadgar@gmail.com"],
+      subject,
+      html,
+    });
+
+    console.log("EMAIL SENT:", info.response);
+  } catch (error) {
+    console.error("EMAIL FAILED:", error);
+  }
 };
